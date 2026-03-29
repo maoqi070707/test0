2cc6dfdd0e55a5f383dfeadf3079d133507a6b29
mq <maoqi070707@163.com>
2026-03-29 16:28:26 +0800

    添加了变量的定义和使用示例，包括整数、浮点数、布尔值和字符串类型。

--- hello.go
+++ hello.go
@@ -11,4 +11,23 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(true && true || false)
+
+	var a int = 10
+	fmt.Println(a)
+
+	var b, c int = 10, 20
+	fmt.Println(b + c)
+
+	var d float32 = 3.14
+	fmt.Println(d)
+
+	var e bool = true
+	fmt.Println(e)
+
+	var f string = "hello"
+	fmt.Println(f)
+	//简写
+	g := "world"
+	fmt.Println(f + " " + g)
+
 }
