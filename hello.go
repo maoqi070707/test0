66362d049e77981c7d851bd78d13fe010ce7adaa
mq <maoqi070707@163.com>
2026-03-29 19:28:40 +0800

    添加了循环

--- hello.go
+++ hello.go
@@ -44,4 +44,26 @@ func main() {
 	fmt.Println(math.Sin(n))
 
 	fmt.Println(s)
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 1; i <= 3; i++ {
+		fmt.Print("===")
+	}
+
+	for {
+		println("loop")
+		break
+	}
+
+	for i := 3; i <= 5; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
 }
